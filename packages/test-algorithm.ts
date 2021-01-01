e70b8f1496f2f1b5bd0ce1c501372cd301de5e24
James Liauw <jamesliauw@163.com>
2021-01-01 14:56:33 +0800

    feat: add common

--- packages/test-algorithm.ts
+++ packages/test-algorithm.ts
@@ -6,36 +6,68 @@
  */
 export class TestAlgorithm {
 	/**
-	 * implement bind
+	 * green
 	 *
 	 * @memberof TestAlgorithm
 	 */
-	public bindMethod(): void {
-		Function.prototype._bind = (ctx: any): any => {
-			const self: any = this;
+	public green(): void {
+		console.log('green');
+	}
 
-			return (): any => {
-				self.call(ctx, ...arguments);
-			};
-		}; 
+	/**
+	 * red
+	 *
+	 * @memberof TestAlgorithm
+	 */
+	public red(): void {
+		console.log('red');
+	}
 
-		/**
-		 * a
-		 *
-		 * @param {*} m
-		 * @param {*} n
-		 * @param {*} o
-		 * @returns {*}
-		 */
-		// tslint:disable-next-line:completed-docs
-		function a(m: any, n: any, o: any): any {
-			console.log(this.name + ' ' + m + ' ' + n + ' ' + o);
-		}
-		const b: any = {
-			name: 'king',
-		};
+	/**
+	 * yellow
+	 *
+	 * @memberof TestAlgorithm
+	 */
+	public yellow(): void {
+		console.log('yellow');
+	}
 
-		// ds e
-		a._bind(b)(7, 8, 9);
+	/**
+	 * task
+	 *
+	 * @param {*} timer
+	 * @param {*} light
+	 * @returns {Promise<any>}
+	 * @memberof TestAlgorithm
+	 */
+	public task(timer: any, light: any): Promise<any> {
+		return new Promise((resolve: any, reject: any): any => {
+			setTimeout((): any => {
+				switch (light) {
+					case 'red':
+						this.red();
+						break;
+					case 'yellow':
+						this.yellow();
+						break;
+					case 'green':
+						this.green();
+						break;
+				}
+			}, timer);
+		});
+	}
+
+	/**
+	 * step console green yellow red light
+	 *
+	 * @returns {Promise<any>}
+	 * @memberof TestAlgorithm
+	 */
+	public async step(): Promise<any> {
+		await this.task(3000, 'red');
+		await this.task(1000, 'green');
+		await this.task(2000, 'yellow');
+		this.step();
 	}
 }
