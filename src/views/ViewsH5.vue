f8cf3288af6fb125781f3aa1edfd4accb1e76a98
James Liauw <jamesliauw@163.com>
2021-02-09 06:02:52 +0800

    feat: add class rules name

--- src/views/ViewsH5.vue
+++ src/views/ViewsH5.vue
@@ -1,24 +1,24 @@
 <template>
 	<div>
 		<!-- top-nav -->
-		<div class="cars-navbar">
+		<div class="cars-views-h5-navbar">
 			<h1>Orders</h1>
-			<div class="cars-nav-container">
+			<div class="cars-views-h5-nav-container">
 				<p><img src="@/assets/slices13@2x.png" /></p>
 				<p><img src="@/assets/slices12@2x.png" /></p>
-				<div class="cars-plus-btn">+</div>
+				<div class="cars-views-h5-plus-btn">+</div>
 			</div>
 		</div>
 		<!-- search container -->
-		<div class="cars-seearch-container">
+		<div class="cars-views-h5-seearch-container">
 			<form>
-				<img class="cars-search-button" src="@/assets/slices11@2x.png" />
-				<input class="cars-seearch" type="text" name="search" placeholder="Search Orders" />
+				<img class="cars-views-h5-search-button" src="@/assets/slices11@2x.png" />
+				<input class="cars-views-h5-seearch" type="text" name="search" placeholder="Search Orders" />
 			</form>
 		</div>
-		<!-- <div class="cars-ad">广告层</div> -->
-		<!-- <div class="cars-product-list">产品列表</div> -->
-		<!-- <div class="cars-chat-box">对话层</div> -->
+		<!-- <div class="cars-views-h5-ad">广告层</div> -->
+		<!-- <div class="cars-views-h5-product-list">产品列表</div> -->
+		<!-- <div class="cars-views-h5-chat-box">对话层</div> -->
 	</div>
 </template>
 
