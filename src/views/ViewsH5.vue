57f5b6b17783d96d55f63ba3c67ec329d57df89d
James Liauw <jamesliauw@163.com>
2021-02-08 20:55:04 +0800

    feat: search input

--- src/views/ViewsH5.vue
+++ src/views/ViewsH5.vue
@@ -1,5 +1,6 @@
 <template>
 	<div>
+		<!-- top-nav -->
 		<div class="cars-navbar">
 			<h1>Orders</h1>
 			<div class="cars-nav-container">
@@ -8,10 +9,16 @@
 				<div class="cars-plus-btn">+</div>
 			</div>
 		</div>
-		<!-- <div class="cars-seearch">搜索层</div>
-		<div class="cars-ad">广告层</div>
-		<div class="cars-product-list">产品列表</div>
-		<div class="cars-chat-box">对话层</div> -->
+		<!-- search container -->
+		<div class="cars-seearch-container">
+			<form>
+				<img class="cars-search-button" src="@/assets/slices11@2x.png" />
+				<input class="cars-seearch" type="text" name="search" placeholder="Search Orders" />
+			</form>
+		</div>
+		<!-- <div class="cars-ad">广告层</div> -->
+		<!-- <div class="cars-product-list">产品列表</div> -->
+		<!-- <div class="cars-chat-box">对话层</div> -->
 	</div>
 </template>
 
