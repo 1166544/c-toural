6a6512f7de704904409e12392ba80cc820502ada
James Liauw <jamesliauw@163.com>
2021-02-11 11:04:36 +0800

    feat: nav top

--- src/components/fashion-show/FashionShow.vue
+++ src/components/fashion-show/FashionShow.vue
@@ -44,8 +44,28 @@
 		</div>
 		<!-- right container -->
 		<div class="cards-fashion-show-right">
-			<div class="cards-fashion-show-right-top">top</div>
-			<div class="cards-fashion-show-right-banner">banner</div>
+			<div class="cards-fashion-show-right-top">
+				<div class="cards-fashion-show-right-top-search-box">
+					<form>
+						<img src="@/assets/fashion/search.png" />
+						<input placeholder="Enter search content here" />
+					</form>
+				</div>
+				<div class="cards-fashion-show-right-top-right">
+					<img src="@/assets/fashion/blockrect.png" />
+					<img src="@/assets/fashion/mrrect.png" />
+					<div class="cards-fashion-show-right-top-text">
+						<h1>Michal Rome</h1>
+						<h2>mrome@valuelogic.com</h2>
+					</div>
+					<div class="cards-fashion-show-right-top-right-arrow">
+						<img src="@/assets/fashion/arrowdown.png" />
+					</div>
+				</div>
+			</div>
+			<div class="cards-fashion-show-right-banner">
+				
+			</div>
 			<div class="cards-fashion-show-right-sub-nav">sub-nav</div>
 			<div class="cards-fashion-show-right-sub-nav">product</div>
 			<div class="cards-fashion-show-right-sub-nav">bottom</div>
