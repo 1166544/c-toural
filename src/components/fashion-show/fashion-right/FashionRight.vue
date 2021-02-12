c456836a0b402ea4b45cfcc194d3088f6d3a3c35
James Liauw <jamesliauw@163.com>
2021-02-12 19:31:44 +0800

    feat: split components

---
+++ src/components/fashion-show/fashion-right/FashionRight.vue
@@ -0,0 +1,343 @@
+<template>
+	<div class="cards-fashion-show-right">
+		<div class="cards-fashion-show-right-top">
+			<div class="cards-fashion-show-right-top-search-box">
+				<form>
+					<img src="@/assets/fashion/search.png" />
+					<input placeholder="Enter search content here" />
+				</form>
+			</div>
+			<div class="cards-fashion-show-right-top-right">
+				<img src="@/assets/fashion/blockrect.png" />
+				<img src="@/assets/fashion/mrrect.png" />
+				<div class="cards-fashion-show-right-top-text">
+					<h1>Michal Rome</h1>
+					<h2>mrome@valuelogic.com</h2>
+				</div>
+				<div class="cards-fashion-show-right-top-right-arrow">
+					<img src="@/assets/fashion/arrowdown.png" />
+				</div>
+			</div>
+		</div>
+		<!-- banner -->
+		<div class="cards-fashion-show-right-banner">
+			<div class="cards-fashion-show-right-banner-container">
+				<img src="@/assets/fashion/banner.png" />
+			</div>
+			<div class="cards-fashion-show-right-banner-box">
+				<div class="cards-fashion-show-right-banner-title">
+					<h1>Trackthor plugins</h1>
+					<img src="@/assets/fashion/arrowdown2.png" />
+				</div>
+				<ul class="cards-fashion-show-right-banner-right">
+					<li>
+						<a>
+							<div class="cards-fashion-show-right-banner-li cards-fashion-show-right-banner-blue">MR</div>
+							<div class="cards-fashion-show-right-banner-li-right">
+								<div class="cards-fashion-show-right-banner-li-top">Michal Rome</div>
+								<div class="cards-fashion-show-right-banner-li-bottom">Senior UI Designer</div>
+							</div>
+						</a>
+					</li>
+					<li>
+						<a>
+							<div class="cards-fashion-show-right-banner-li cards-fashion-show-right-banner-green">DK</div>
+							<div class="cards-fashion-show-right-banner-li-right">
+								<div class="cards-fashion-show-right-banner-li-top">Dominika Kropp</div>
+								<div class="cards-fashion-show-right-banner-li-bottom">Visual Designer</div>
+							</div>
+						</a>
+					</li>
+					<li>
+						<a>
+							<div class="cards-fashion-show-right-banner-li cards-fashion-show-right-banner-purple">MT</div>
+							<div class="cards-fashion-show-right-banner-li-right">
+								<div class="cards-fashion-show-right-banner-li-top">Marcin Twardowski</div>
+								<div class="cards-fashion-show-right-banner-li-bottom">UX Designer</div>
+							</div>
+						</a>
+					</li>
+				</ul>
+			</div>
+		</div>
+		<!-- sub-nav -->
+		<ul class="cards-fashion-show-right-sub-nav">
+			<li>
+				<a>
+					<img src="@/assets/fashion/fashion.png" />
+					<div class="cards-fashion-show-right-sub-nav-item">
+						<h1>QUALITY MATERIALS</h1>
+						<p>Gurnte 100% polyuretuned and 100% popular</p>
+					</div>
+				</a>
+			</li>
+			<li>
+				<a>
+					<img src="@/assets/fashion/shipping.png" />
+					<div class="cards-fashion-show-right-sub-nav-item">
+						<h1>FREE SHIPPING</h1>
+						<p>We free shipping for all offers over $199</p>
+					</div>
+				</a>
+			</li>
+			<li>
+				<a>
+					<img src="@/assets/fashion/shell.png" />
+					<div class="cards-fashion-show-right-sub-nav-item">
+						<h1>SECURE PAYMENT</h1>
+						<p>George 100% pure payment online on our website</p>
+					</div>
+				</a>
+			</li>
+		</ul>
+		<!-- product -->
+		<div class="cards-fashion-show-right-product">
+			<ul class="cards-fashion-show-right-product-ul">
+				<li>
+					<a>
+						<img src="@/assets/fashion/hat.png" />
+						<h1>Classic Hat</h1>
+					</a>
+				</li>
+				<li>
+					<a>
+						<img src="@/assets/fashion/wallet.png" />
+						<h1>Mini Wallet</h1>
+					</a>
+				</li>
+				<li>
+					<a>
+						<img src="@/assets/fashion/belt.png" />
+						<h1>Belt Bag</h1>
+					</a>
+				</li>
+				<li class="cards-fashion-show-right-product-li">
+					<a>
+						<img src="@/assets/fashion/shoes.png" />
+						<h1>Shoes</h1>
+					</a>
+				</li>
+			</ul>
+			<!-- right content -->
+			<div class="cards-fashion-show-right-product-right">
+				<div class="cards-fashion-show-right-product-right-top">
+					<div class="cards-fashion-show-right-product-right-left">
+						<h1>Client workshops</h1>
+						<h2>10:00 - 17:00</h2>
+					</div>
+					<div class="cards-fashion-show-right-product-right-right">
+						<h1 class="cards-fashion-show-right-banner-purple">MS</h1>
+						<h1 class="cards-fashion-show-right-banner-green">UK</h1>
+						<h1 class="cards-fashion-show-right-banner-blue">+5</h1>
+					</div>
+				</div>
+				<div class="cards-fashion-show-right-product-right-bottom">
+					<div class="cards-fashion-show-right-product-right-bottom-box">
+						<h1 class="cards-fashion-show-right-product-right-bottom-box-h1">November</h1>
+						<ul class="cards-fashion-show-right-product-right-bottom-box-ul">
+							<li class="cards-fashion-show-right-product-right-bottom-box-ul-li">
+								<a>
+									<h1 class="cards-fashion-show-right-product-right-bottom-box-item">16</h1>
+									<h2>Fri</h2>
+								</a>
+							</li>
+							<li>
+								<a>
+									<h1 class="cards-fashion-show-right-product-right-bottom-box-item-style">17</h1>
+									<h2>Sat</h2>
+								</a>
+							</li>
+							<li>
+								<a>
+									<h1 class="cards-fashion-show-right-product-right-bottom-box-item-style">18</h1>
+									<h2>Sun</h2>
+								</a>
+							</li>
+						</ul>
+					</div>
+					<div class="cards-fashion-show-right-product-right-bottom-box cards-fashion-show-right-product-right-bottom-box-copy">
+						<h1 class="cards-fashion-show-right-product-right-bottom-box-h1 cards-fashion-show-right-product-right-bottom-box-title">
+							December
+						</h1>
+						<ul class="cards-fashion-show-right-product-right-bottom-box-ul">
+							<li class="cards-fashion-show-right-product-right-bottom-box-ul-li">
+								<a>
+									<h1
+										class="cards-fashion-show-right-product-right-bottom-box-item cards-fashion-show-right-product-right-bottom-box-item-copy"
+									>
+										19
+									</h1>
+									<h2>Mon</h2>
+								</a>
+							</li>
+							<li>
+								<a>
+									<h1
+										class="cards-fashion-show-right-product-right-bottom-box-item-style cards-fashion-show-right-product-right-bottom-box-item-copy cards-fashion-show-right-product-right-bottom-box-item-border"
+									>
+										20
+									</h1>
+									<h2>Tue</h2>
+								</a>
+							</li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+		<!-- bottom -->
+		<div class="cards-fashion-show-right-bottom">
+			<div class="cards-fashion-show-right-bottom-top">
+				<h1>Good morning,Michal</h1>
+				<p>Here whats happening in yur VL account today</p>
+			</div>
+			<ul class="cards-fashion-show-right-bottom-bottom">
+				<li>
+					<div class="cards-fashion-show-right-bottom-bottom-item">
+						<div class="cards-fashion-show-right-bottom-bottom-item-title">
+							<h1>Long Time</h1>
+							<h2>+</h2>
+						</div>
+						<ul>
+							<li class="cards-fashion-show-right-bottom-bottom-item-spa">
+								<a class="cards-fashion-show-right-bottom-bottom-item-li">
+									<div class="cards-fashion-show-right-bottom-bottom-item-li-left">
+										<div class="cards-fashion-show-right-bottom-bottom-item-li-round">
+											<img src="@/assets/fashion/clock.png" />
+										</div>
+										<h1>VLweb_203</h1>
+									</div>
+									<h2>4H</h2>
+								</a>
+							</li>
+							<li class="cards-fashion-show-right-bottom-bottom-item-spa">
+								<a class="cards-fashion-show-right-bottom-bottom-item-li">
+									<div class="cards-fashion-show-right-bottom-bottom-item-li-left">
+										<div class="cards-fashion-show-right-bottom-bottom-item-li-round">
+											<img src="@/assets/fashion/clock.png" />
+										</div>
+										<h1>VLweb_204</h1>
+									</div>
+									<h2>2H</h2>
+								</a>
+							</li>
+							<li class="cards-fashion-show-right-bottom-bottom-item-spa">
+								<a class="cards-fashion-show-right-bottom-bottom-item-li">
+									<div class="cards-fashion-show-right-bottom-bottom-item-li-left">
+										<div class="cards-fashion-show-right-bottom-bottom-item-li-round">
+											<img src="@/assets/fashion/clock.png" />
+										</div>
+										<h1>VLweb_205</h1>
+									</div>
+									<h2>1H</h2>
+								</a>
+							</li>
+						</ul>
+					</div>
+				</li>
+				<li>
+					<div class="cards-fashion-show-right-bottom-bottom-item">
+						<div class="cards-fashion-show-right-bottom-bottom-item-title">
+							<h1>Calendar</h1>
+							<h2>+</h2>
+						</div>
+						<ul>
+							<li class="cards-fashion-show-right-bottom-bottom-item-spa cards-fashion-show-right-bottom-bottom-item-spa-blue">
+								<a class="cards-fashion-show-right-bottom-bottom-item-li">
+									<div class="cards-fashion-show-right-bottom-bottom-item-li-left">
+										<div class="cards-fashion-show-right-bottom-bottom-item-li-round">
+											<img src="@/assets/fashion/schedule.png" />
+										</div>
+										<h1>Webflow Trainings</h1>
+									</div>
+									<h2>22:11</h2>
+								</a>
+							</li>
+							<li class="cards-fashion-show-right-bottom-bottom-item-spa cards-fashion-show-right-bottom-bottom-item-spa-blue">
+								<a class="cards-fashion-show-right-bottom-bottom-item-li">
+									<div class="cards-fashion-show-right-bottom-bottom-item-li-left">
+										<div class="cards-fashion-show-right-bottom-bottom-item-li-round">
+											<img src="@/assets/fashion/schedule.png" />
+										</div>
+										<h1>Dominika 1-1</h1>
+									</div>
+									<h2>01.12</h2>
+								</a>
+							</li>
+							<li class="cards-fashion-show-right-bottom-bottom-item-spa cards-fashion-show-right-bottom-bottom-item-spa-blue">
+								<a class="cards-fashion-show-right-bottom-bottom-item-li">
+									<div class="cards-fashion-show-right-bottom-bottom-item-li-left">
+										<div class="cards-fashion-show-right-bottom-bottom-item-li-round">
+											<img src="@/assets/fashion/schedule.png" />
+										</div>
+										<h1>Call with Krzysiek</h1>
+									</div>
+									<h2>05.12</h2>
+								</a>
+							</li>
+						</ul>
+					</div>
+				</li>
+				<li>
+					<div class="cards-fashion-show-right-bottom-bottom-item">
+						<div class="cards-fashion-show-right-bottom-bottom-item-title">
+							<h1>Documents</h1>
+							<h2></h2>
+						</div>
+						<ul>
+							<li class="cards-fashion-show-right-bottom-bottom-item-spa cards-fashion-show-right-bottom-bottom-item-spa-green">
+								<a class="cards-fashion-show-right-bottom-bottom-item-li">
+									<div class="cards-fashion-show-right-bottom-bottom-item-li-left">
+										<div class="cards-fashion-show-right-bottom-bottom-item-li-round">
+											<img src="@/assets/fashion/document.png" />
+										</div>
+										<h1>Timesheet.doc</h1>
+									</div>
+									<h2>11kb</h2>
+								</a>
+							</li>
+							<li class="cards-fashion-show-right-bottom-bottom-item-spa cards-fashion-show-right-bottom-bottom-item-spa-green">
+								<a class="cards-fashion-show-right-bottom-bottom-item-li">
+									<div class="cards-fashion-show-right-bottom-bottom-item-li-left">
+										<div class="cards-fashion-show-right-bottom-bottom-item-li-round">
+											<img src="@/assets/fashion/document.png" />
+										</div>
+										<h1>Business Value.pdf</h1>
+									</div>
+									<h2>2.45mb</h2>
+								</a>
+							</li>
+							<li class="cards-fashion-show-right-bottom-bottom-item-spa cards-fashion-show-right-bottom-bottom-item-spa-green">
+								<a class="cards-fashion-show-right-bottom-bottom-item-li">
+									<div class="cards-fashion-show-right-bottom-bottom-item-li-left">
+										<div class="cards-fashion-show-right-bottom-bottom-item-li-round">
+											<img src="@/assets/fashion/document.png" />
+										</div>
+										<h1>Client Proposal.pdf</h1>
+									</div>
+									<h2>54kb</h2>
+								</a>
+							</li>
+						</ul>
+					</div>
+				</li>
+			</ul>
+		</div>
+		<div class="cards-fashion-show-copyrights">
+			Copy rights @2021 trackthor all rights reserved.
+		</div>
+	</div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import './FashionRight.less';
+
+/** fashion left */
+@Component
+export default class FashionRight extends Vue {
+	constructor() {
+		super();
+	}
+}
+</script>
