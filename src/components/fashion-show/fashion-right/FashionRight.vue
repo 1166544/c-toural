2e45d288aa48ebb0eb3db5dca28d8a52a8937908
James Liauw <jamesliauw@163.com>
2021-02-12 20:56:30 +0800

    feat: bind years value

--- src/components/fashion-show/fashion-right/FashionRight.vue
+++ src/components/fashion-show/fashion-right/FashionRight.vue
@@ -16,14 +16,12 @@
 		<FashionBottom />
 
 		<!-- copyright -->
-		<div class="cards-fashion-show-copyrights">
-			Copy rights @2021 trackthor all rights reserved.
-		</div>
+		<div class="cards-fashion-show-copyrights">Copy rights @{{ years }} trackthor all rights reserved.</div>
 	</div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
+import { Component, Prop, Vue } from 'vue-property-decorator';
 import './FashionRight.less';
 import FashionBanner from '@/components/fashion-show/fashion-right/fashion-banner/FashionBanner.vue';
 import FashionBottom from '@/components/fashion-show/fashion-right/fashion-bottom/FashionBottom.vue';
@@ -42,8 +40,19 @@ import FashionTop from '@/components/fashion-show/fashion-right/fashion-top/Fash
 	}
 })
 export default class FashionRight extends Vue {
+	public yearsValue: string = '';
+
 	constructor() {
 		super();
+	}
+
+	public get years(): string {
+		return this.yearsValue;
+	}
+
+	/** mounted */
+	public mounted(): void {
+		this.yearsValue = new Date().getFullYear().toString();
 	}
 }
 </script>
