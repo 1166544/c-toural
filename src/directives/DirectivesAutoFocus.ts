6b2c9c8905faddc9518628d717935d507d1aea52
James Liauw <jamesliauw@163.com>
2021-02-14 12:46:29 +0800

    feat: extends hook

--- src/directives/DirectivesAutoFocus.ts
+++ src/directives/DirectivesAutoFocus.ts
@@ -1,4 +1,6 @@
 import Vue from 'vue';
+import { DirectiveBinding } from 'vue/types/options';
+import { VNode } from 'vue/types/vnode';
 
 /**
  * auto focus
@@ -8,13 +10,37 @@ import Vue from 'vue';
  */
 class DirectivesAutoFocus extends Vue {
 	/**
-	 * inserted
+	 * bind hook
+	 *
+	 * @param {HTMLElement} el
+	 * @param {DirectiveBinding} binding
+	 * @param {VNode} vnode
+	 * @memberof DirectivesAutoFocus
+	 */
+	public bind(el: HTMLElement, binding: DirectiveBinding, vnode: VNode): void {
+		const innerHTML: any = `
+			name: ${JSON.stringify(binding.name)}
+			value:
+			${JSON.stringify(binding.value)}
+			expression:
+			${JSON.stringify(binding.expression)}
+			'argument: '
+			${JSON.stringify(binding.arg)}
+			modifiers:
+			${JSON.stringify(binding.modifiers)}
+			vnode keys:
+			${Object.keys(vnode).join(', ')}`;
+		console.log(innerHTML);
+	}
+
+	/**
+	 * inserted hook
 	 *
 	 * @param {*} el
 	 * @returns {*}
 	 * @memberof DirectivesAutoFocus
 	 */
-	public inserted(el: HTMLElement): any {
+	public inserted(el: HTMLElement, binding: DirectiveBinding, vnode: VNode, oldVnode: VNode): any {
 		console.log('start focus monitor');
 		if (el) {
 			el.focus();
